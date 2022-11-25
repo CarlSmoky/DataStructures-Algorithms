91b87a9f5631393b67698f3284b2ebf979223a21
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2022-11-25 11:57:58 +0000

    Sync LeetCode submission - Check If Two String Arrays are Equivalent (javascript)

---
+++ problems/check_if_two_string_arrays_are_equivalent/solution.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+const arrayStringsAreEqual = (word1, word2) => {
+  const string1 = word1.join("");
+  const string2 = word2.join("");
+  return string1 === string2;
+};
