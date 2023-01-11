fa5ee20ac7077e189bdd593922b395fabaa792a0
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2023-01-11 15:46:13 +0000

    Sync LeetCode submission - Climbing Stairs (javascript)

---
+++ problems/climbing_stairs/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const cache = {
+  "0": 1,
+  "1": 1,
+};
+
+const climbStairs = function (n) {
+  //base cases
+  if (n <= 1) return 1;
+  for (let i = 2; i <= n; i++) {
+    cache[i] = cache[i - 1] + cache[i - 2];
+  }
+  return cache[n];
+};
