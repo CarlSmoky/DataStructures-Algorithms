7b051f34fff2a08379825b64bc8fb461270c134e
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2023-04-05 15:05:00 +0000

    Sync LeetCode submission - Valid Parentheses (javascript)

--- problems/valid_parentheses/solution.js
+++ problems/valid_parentheses/solution.js
@@ -1,26 +1,27 @@
 /**
  * @param {string} s
- * @
- return {boolean}
+ * @return {boolean}
  */
-var isValid = function(s) {
-    let map = {
-      ")": "(",
-      "]": "[",
-      "}": "{"
-  }
+const brackets = {
+  ')' : '(',
+  '}' : '{',
+  ']' : '['
+}
+const isValid = (s) => {
   let arr = [];
+  const values = Object.values(brackets);
   
-  for(let i = 0; i < s.length; i ++){
-      if(s[i] === "(" || s[i] === "[" || s[i] === "{"){
-          arr.push(s[i]);
-      }
-      else{
-          if(arr[arr.length - 1] === map[s[i]]){
-              arr.pop();
-          }
-          else return false;
-      }
+  for (let i = 0; i < s.length; i++) {
+    if (values.indexOf(s[i]) !== -1) {
+      arr = [...arr, s[i]];
+    } else {
+      if (arr[arr.length -1] === brackets[s[i]]) {
+        arr.pop();
+      } 
+      else return false;
+    }
   }
+  
   return arr.length === 0 ? true : false;
+  
 };
