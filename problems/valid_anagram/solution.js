480dfa35e2f3e29b0ee77aad824157319210f77b
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2023-04-19 13:53:35 +0000

    Sync LeetCode submission - Valid Anagram (javascript)

--- problems/valid_anagram/solution.js
+++ problems/valid_anagram/solution.js
@@ -4,5 +4,22 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    return [...s].sort().join("") === [...t].sort().join("");
+    return [...s].sort().join('') === [...t].sort().join('');
 };
+
+// var isAnagram = function(s, t) {
+// if (s.length !== t.length) return false
+    
+//     const hashTable = {}
+//     for (let letter of s){
+//         if (!hashTable[letter]) hashTable[letter] = 0;
+//         hashTable[letter]++;
+//     }
+
+//     for (let letter of t){
+//         if (!hashTable[letter]) return false;
+//         hashTable[letter]--;
+//     }
+
+//     return true;
+// }
