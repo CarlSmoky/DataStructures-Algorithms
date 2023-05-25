8cd7bc3fcf482ae2595bcd96a44e70323db7e455
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2023-05-25 18:53:07 +0000

    Sync LeetCode submission - Find Pivot Index (javascript)

--- problems/find_pivot_index/solution.js
+++ problems/find_pivot_index/solution.js
@@ -2,22 +2,25 @@
  * @param {number[]} nums
  * @return {number}
  */
-var pivotIndex = function(nums) {
+// const pivotIndex = (nums) => {
+//     for (let tmpPivotIndex = 0; tmpPivotIndex < nums.length; tmpPivotIndex++) {
+//         const left = nums.slice(0, tmpPivotIndex);
+//         const right = nums.slice(tmpPivotIndex + 1);
+//         const leftSum = left.reduce((acc, cur) => acc + cur, 0);
+//         const rightSum = right.reduce((acc, cur) => acc + cur, 0);
+//         if (leftSum === rightSum) return tmpPivotIndex;
+//     }
+//     return -1;
+// };
+
+const pivotIndex = (nums) => {
+    const total = nums.reduce((acc, cur) => acc + cur, 0);
     let leftSum = 0;
-  for (let i = 0; i < nums.length; i++) {
-    let rightSum = 0;
-    if (i === 0) {
-      for (let j = 1; j < nums.length; j++) {
-        rightSum += nums[j];
-      }
-      if (leftSum === rightSum) return 0;
-    } else {
-      leftSum += nums[i - 1];
-      for (let j = i + 1; j < nums.length; j++) {
-        rightSum += nums[j];
-      }
-      if (leftSum === rightSum) return i;
+    for (let i = 0; i < nums.length; i++) {
+        const rightSum = total - leftSum - nums[i];
+        if (leftSum === rightSum) return i;
+        leftSum += nums[i];
     }
-  }
-  return -1;
+    return -1;
 };
+    
