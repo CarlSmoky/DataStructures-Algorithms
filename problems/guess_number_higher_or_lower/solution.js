1fbd6995a4d7216b4bc1cd7cf8fed9d1677bc016
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2023-05-26 16:42:41 +0000

    Sync LeetCode submission - Guess Number Higher or Lower (javascript)

---
+++ problems/guess_number_higher_or_lower/solution.js
@@ -0,0 +1,25 @@
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const guessNumber = n => {
+    
+    let left = 1;
+    let right = n;
+
+    while(left < right){
+        let mid = left + Math.floor((right - left) / 2);
+        if(guess(mid) <= 0) right = mid;
+        else left = mid + 1;
+    }
+    return left;
+};
