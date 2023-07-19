d747214d16c9fd9eb226bdbf4f95bc1633e6ee89
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2023-07-19 14:33:05 +0000

    Sync LeetCode submission - Apply Transform Over Each Element in Array (javascript)

--- problems/apply_transform_over_each_element_in_array/solution.js
+++ problems/apply_transform_over_each_element_in_array/solution.js
@@ -6,7 +6,7 @@
 const map = (arr, fn) => {
     let result = [];
     for (let i = 0; i < arr.length; i++) {
-        result.push(fn(arr[i], i));
+        result.push(fn(arr[i] , i));
     }
     return result;
 };
