69a4769ebc729526b72f95cdfd1ebc90955bf1f4
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2023-07-21 14:30:27 +0000

    Sync LeetCode submission - Longest Common Prefix (javascript)

--- problems/longest_common_prefix/solution.js
+++ problems/longest_common_prefix/solution.js
@@ -2,14 +2,28 @@
  * @param {string[]} strs
  * @return {string}
  */
-const longestCommonPrefix = (strs) => {
-  if (!strs.length) return "";
-  for (let i = 0; i < strs[0].length; i++) {
-    for (let s of strs) {
-      if (strs[0][i] !== s[i]) {
-        return s.slice(0, i);
-      }
+// const longestCommonPrefix = strs => {
+//     for (let i = 0; i < strs[0].length; i++) {
+//         for (let j = 1; j < strs.length; j++) {
+//             if (strs[0][i] !== strs[j][i]) {
+//                 return strs[0].slice(0, i)
+//             }
+//         }
+//     }
+//     return strs[0];
+// };
+
+const longestCommonPrefix = strs => {
+    let i = 1;
+    let prf = strs[0];
+    while(i < strs.length){
+        console.log(i)
+        console.log(prf)
+        if(!strs[i].startsWith(prf)){
+            prf = prf.slice(0, -1)
+        }else{
+            i++
+        }
     }
-  }
-  return strs[0];
-}
+    return prf
+};
