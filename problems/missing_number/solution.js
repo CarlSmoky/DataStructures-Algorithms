3181655c2d55d13542faee88cdfa1c22115611e9
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2023-07-31 15:40:05 +0000

    Sync LeetCode submission - Missing Number (javascript)

--- problems/missing_number/solution.js
+++ problems/missing_number/solution.js
@@ -3,10 +3,12 @@
  * @return {number}
  */
 const missingNumber = (nums) => {
-  // Get max number
-  const maxNum = Math.max(...nums);
-  // Iterate nums from 0 to max num and check if there is missiong num
-  for (let i = 0; i < nums.length + 1; i++) {
-    if (nums.indexOf(i) === -1) return i;
-  }
+   const newNums = nums.sort((a, b) => a - b);
+   let arr = [];
+   for (let i = 0; i <= nums.length; i++) {
+      arr.push(i);
+   }
+   for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== newNums[i]) return arr[i];
+   }
 };
