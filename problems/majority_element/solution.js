849c32f91f088e10944f30cb8003b1e168b5dee5
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2023-08-11 14:54:51 +0000

    Sync LeetCode submission - Majority Element (javascript)

--- problems/majority_element/solution.js
+++ problems/majority_element/solution.js
@@ -3,12 +3,10 @@
  * @return {number}
  */
 const majorityElement = (nums) => {
-    //  map nums and count as value
-    let map = {};
+    // make hashMap with count
+    const map = {}
     for (let i = 0; i < nums.length; i++) {
-        const current = nums[i];
-        map[current] = map[current] ? map[current] + 1 : 1;
-        // if  count is bigger that n / 2, return key
-        if (map[current] > nums.length / 2) return current;
+        map[nums[i]] = map[nums[i]] ? map[nums[i]] + 1 : 1;
+        if (map[nums[i]] > nums.length / 2) return nums[i]
     }
 };
