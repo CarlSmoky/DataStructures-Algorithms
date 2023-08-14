a5a6b681eefcba7b543f052d4551a32c825a4b0a
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2023-08-14 13:47:04 +0000

    Sync LeetCode submission - Length of Last Word (javascript)

--- problems/length_of_last_word/solution.js
+++ problems/length_of_last_word/solution.js
@@ -2,7 +2,7 @@
  * @param {string} s
  * @return {number}
  */
-const lengthOfLastWord = s => {
+const lengthOfLastWord = (s) => {
     const arr = s.trim().split(" ");
     return arr[arr.length - 1].length;
 };
