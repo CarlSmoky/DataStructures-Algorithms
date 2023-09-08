fa87cf1cc91e689a7876cf3783942cc6957113c3
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2023-09-08 14:06:34 +0000

    Sync LeetCode submission - Kids With the Greatest Number of Candies (javascript)

--- problems/kids_with_the_greatest_number_of_candies/solution.js
+++ problems/kids_with_the_greatest_number_of_candies/solution.js
@@ -4,11 +4,6 @@
  * @return {boolean[]}
  */
 const kidsWithCandies = (candies, extraCandies) => {
-   //get max number of candy
-   const max = Math.max(...candies);
-   //return array contains boolean for each el if el has more than mac num of candy 
-   return candies.map(el => {
-      const tmp = el + extraCandies;
-      return tmp >= max;
-   })
+    const currentMax = Math.max(...candies);
+    return candies.map(el => el + extraCandies >= currentMax)
 };
