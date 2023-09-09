518e78dc6ec0b47fdce9526bd6095dafb123c688
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2023-09-09 22:48:20 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock (javascript)

--- problems/best_time_to_buy_and_sell_stock/solution.js
+++ problems/best_time_to_buy_and_sell_stock/solution.js
@@ -2,18 +2,22 @@
  * @param {number[]} prices
  * @return {number}
  */
-const maxProfit = (prices) => {
-    let l = 0;
-    let r = l + 1;
-    let maxProfit = 0;
-    while (l < prices.length) {
-        if (prices[l] < prices[r]) {
-            const tmpProfit =  prices[r] - prices[l];
-            maxProfit = Math.max(tmpProfit, maxProfit);
+const maxProfit = prices => {
+    let maxDiff = 0;
+    let left = 0;
+    let right = 1;
+    while (left < prices.length) {
+        console.log("l", left)
+        console.log("r", right)
+        if (prices[left] < prices[right]) {
+            const tmpDiff = prices[right] - prices[left];
+            maxDiff = Math.max(maxDiff, tmpDiff)
         } else {
-            l = r;
+            // e.g [7,1,5,3,6,4]
+            // 7 > 1, so give up on 7, because 1 will always have a better solution
+            left = right;
         }
-        r++;
+        right++;
     }
-    return maxProfit;
+    return maxDiff;
 };
