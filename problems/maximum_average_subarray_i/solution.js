449d87ff14a4bdcce62d785991bcd7392ee11597
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2023-09-30 19:11:31 +0000

    Sync LeetCode submission - Maximum Average Subarray I (javascript)

--- problems/maximum_average_subarray_i/solution.js
+++ problems/maximum_average_subarray_i/solution.js
@@ -4,17 +4,19 @@
  * @return {number}
  */
 const findMaxAverage = (nums, k) => {
-    // find max sum of k length of num
-    let tmpmax = 0;
-    let tmpSum = 0;
-    for (let i = 0; i < k; i++){
-        tmpmax += nums[i];
-    }
-    tmpSum = tmpmax; 
-    for (let i = k; i < nums.length; i++) {
-        tmpSum = (tmpSum - nums[i - k] + nums[i]);
-        tmpmax = Math.max(tmpmax, tmpSum);
+    let average = -Infinity;
+    let sum = 0;
+    let start = 0;
+    
+    for(let end = 0; end < nums.length; end++){
+         sum += nums[end];
+        
+        if((end - start) + 1 === k){
+            average = Math.max(average, sum/k);
+            sum -= nums[start];
+            start += 1;
+        }       
     }
     
-    return tmpmax / k;
+    return average;
 };
