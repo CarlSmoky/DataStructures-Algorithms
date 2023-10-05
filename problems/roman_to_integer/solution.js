140a0d82f4300801becab8d10d904aa0a179c2ed
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2023-10-05 18:12:20 +0000

    Sync LeetCode submission - Roman to Integer (javascript)

--- problems/roman_to_integer/solution.js
+++ problems/roman_to_integer/solution.js
@@ -2,18 +2,16 @@
  * @param {string} s
  * @return {number}
  */
-const romanToInt = s => {
+const romanToInt = (s) => {
     const roman = {"I" : 1,"V": 5, "X": 10, "L": 50, "C": 100, "D": 500,"M":1000}
     let sum = 0;
     for (let i = 0; i < s.length; i++) {
-        let curr = roman[s[i]];
-        let next = roman[s[i + 1]];
-        if (curr < next) {
-            sum += next - curr;
-            i++;
+        if (roman[s[i]] < roman[s[i + 1]]) {
+            sum += roman[s[i + 1]] - roman[s[i]]
+            i++
         } else {
-            sum += curr;
+            sum += roman[s[i]]
         }
     }
-    return sum;
+    return sum
 };
