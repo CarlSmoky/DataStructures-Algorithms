9d9f697307bff04bd28c348bbad2d95af48d145d
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2023-10-14 07:46:43 +0000

    Sync LeetCode submission - Merge Strings Alternately (javascript)

--- problems/merge_strings_alternately/solution.js
+++ problems/merge_strings_alternately/solution.js
@@ -3,38 +3,22 @@
  * @param {string} word2
  * @return {string}
  */
-// const mergeAlternately = (word1, word2) => {
-//     //Adjust length of word and get left over 
-//     let leftOver = ""
-//     if (word1.length > word2.length) {
-//         leftOver = word1.slice(word2.length);
-//         word1 = word1.slice(0, word2.length);
-//     } else {
-//         leftOver = word2.slice(word1.length);
-//         word2 = word2.slice(0, word1.length);
-//     }
-    
-//     //define margedString
-//     let margedString = ""
-//     //iterate through word1
-//     for (let i = 0; i < word1.length; i++) {
-//         margedString += word1[i];
-//         margedString += word2[i];
-//     }
-//     //append the left over to margedString
-//     margedString += leftOver;
-//     // return margedString
-//     return margedString;
-// };
-//Refactor
 const mergeAlternately = (word1, word2) => {
-    let i = 0;
-    let newStr = ""
-    while (i < word1.length && i < word2.length) {
-        newStr += word1[i] + word2[i];
-        i++;
+    let left = 0
+    let right = 0
+    let result = ''
+    while (left < word1.length || right < word2.length) {
+        if (left < word1.length && right < word2.length) {
+            result += word1[left]
+            result += word2[right]
+        } else if (left < word1.length && right >= word2.length) {
+            result += word1[left]
+        } else if (left >= word1.length && right < word2.length) {
+            result += word2[right]
+        }
+        left ++
+        right ++
     }
-    const leftover = word1.length > word2.length ? word1.slice(word2.length) : word2.slice(word1.length);
-    return newStr + leftover;
-}
-
+    return result
+    
+};
