653be48eb821515fe1234a5c137d635784e9e54f
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2023-10-20 01:40:46 +0000

    Sync LeetCode submission - Two Sum (javascript)

--- problems/two_sum/solution.js
+++ problems/two_sum/solution.js
@@ -4,19 +4,13 @@
  * @return {number[]}
  */
 const twoSum = (nums, target) => {
-    // hashmap
-    const hashMap = {};
-    // iterate through nums
+    let map = {}
     for (let i = 0; i < nums.length; i++) {
-        // diff = target - nums[i]
-        const diff = target - nums[i];
-        // check if diff in hashmap
-        // if exists, return value of the key , i
-        if (hashMap[diff] || hashMap[diff] === 0) {
-            return [hashMap[diff], i];
-        // if not, add key as nums[i] and value as i
+        const diff = target - nums[i]
+        if (map[diff] || map[diff] === 0) {
+            return [map[diff], i]
         } else {
-            hashMap[nums[i]] = i
+            map[nums[i]] = i
         }
     }
 };
