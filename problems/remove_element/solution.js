8accdcd0f1e99b43a9e809f58c9cdc2a06bb13e3
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2023-11-16 15:01:50 +0000

    Sync LeetCode submission - Remove Element (javascript)

--- problems/remove_element/solution.js
+++ problems/remove_element/solution.js
@@ -4,12 +4,10 @@
  * @return {number}
  */
 const removeElement = (nums, val) => {
-    // iterate through nums
     for (let i = 0; i < nums.length; i++) {
-    // if the el of nums is the same val, splice the el and i minus 1 
         if (nums[i] === val) {
-            nums.splice(i, 1);
-            i -= 1;
+            nums.splice(i, 1)
+            i--
         }
-    } 
+    }
 };
