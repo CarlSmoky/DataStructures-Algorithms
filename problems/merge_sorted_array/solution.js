9f200d2e1f30a5d4a682d38d1e497205435d9c08
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2023-11-16 14:52:54 +0000

    Sync LeetCode submission - Merge Sorted Array (javascript)

--- problems/merge_sorted_array/solution.js
+++ problems/merge_sorted_array/solution.js
@@ -6,17 +6,17 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 const merge = (nums1, m, nums2, n) => {
-    let left = m - 1;
-    let right = n - 1;
-    let i = m + n - 1;
-    while (right >= 0) {
-        if (nums1[left] > nums2[right]) {
-            nums1[i] = nums1[left];
-            left --;
+    let first = m - 1
+    let second = n - 1
+    let i = m + n - 1
+    while ( second >= 0 ) {
+        if (nums1[first] > nums2[second]) {
+            nums1[i] = nums1[first]
+            first --
         } else {
-            nums1[i] = nums2[right];
-            right --;
+            nums1[i] = nums2[second]
+            second --
         }
-        i--;
+        i--
     }
 };
