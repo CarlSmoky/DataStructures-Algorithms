07b6ff35f2fcd4be5d59eb0e64edb1f98a46d41c
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-02-21 16:06:27 +0000

    Sync LeetCode submission - Contains Duplicate (python3)

---
+++ problems/contains_duplicate/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hash = {}
+        for n in nums:
+            if n in hash:
+                return True
+            hash[n] = 1
+        return False
