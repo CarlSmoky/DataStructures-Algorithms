ffb7f8299d39583837a04174825af33ffce50306
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-05-07 10:06:52 -0400

    Time: 47 ms (92.54%) | Memory: 50.5 MB (22.24%) - LeetSync

--- problems/1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.js
+++ problems/1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.js
@@ -3,13 +3,17 @@
  * @return {number}
  */
 const maxProduct = (nums) => {
-    if (nums.length === 2) return (nums[0] - 1) * (nums[1] - 1);
-    let maxProduct = 0;
-    for (let i = 0; i < nums.length - 1; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            const tmpProduct = (nums[i] - 1) * (nums[j] - 1);
-            maxProduct = Math.max(maxProduct, tmpProduct);
-        }
-    }
-    return maxProduct;
+    // Brute force
+    // if (nums.length === 2) return (nums[0] - 1) * (nums[1] - 1);
+    // let maxProduct = 0;
+    // for (let i = 0; i < nums.length - 1; i++) {
+    //     for (let j = i + 1; j < nums.length; j++) {
+    //         const tmpProduct = (nums[i] - 1) * (nums[j] - 1);
+    //         maxProduct = Math.max(maxProduct, tmpProduct);
+    //     }
+    // }
+    // return maxProduct;
+
+    const sortedNums = nums.sort((a, b) => b - a);
+    return (sortedNums[0] - 1) * (sortedNums[1] - 1);
 };
