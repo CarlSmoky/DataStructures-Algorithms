36462dc0e6a0e975c3463e4d80dc521e0bcd8877
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-06-04 10:39:16 -0400

    Time: 56 ms (77.83%) | Memory: 49.7 MB (60.87%) - LeetSync

--- problems/3379-score-of-a-string/score-of-a-string.js
+++ problems/3379-score-of-a-string/score-of-a-string.js
@@ -5,9 +5,7 @@
 const scoreOfString = (s) => {
     let score = 0;
     for (let i = 0; i < s.length - 1; i++) {
-        const fisrtCharCode = s.charCodeAt(i);
-        const secondCharCode = s.charCodeAt(i + 1);
-        score += Math.abs(fisrtCharCode - secondCharCode);
+        score += Math.abs(s.charCodeAt(i) - s.charCodeAt(i + 1));
     }
     return score;
     
