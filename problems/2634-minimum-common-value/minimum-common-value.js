7767a9bbb8796512da782095789c3bc46bb1b221
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-06-07 15:47:13 -0400

    Time: 63 ms (81.53%) | Memory: 56.6 MB (67.74%) - LeetSync

--- problems/2634-minimum-common-value/minimum-common-value.js
+++ problems/2634-minimum-common-value/minimum-common-value.js
@@ -4,9 +4,19 @@
  * @return {number}
  */
 const getCommon = (nums1, nums2) => {
-    const setNums1 = new Set(nums1);
-    for (let n of nums2) {
-        if (setNums1.has(n)) return n;
+    // Set
+    // const setNums1 = new Set(nums1);
+    // for (let n of nums2) {
+    //     if (setNums1.has(n)) return n;
+    // }
+    // return -1;
+
+    // Two pointer
+    let firstIndex = 0;
+    let secondIndex = 0;
+    while (firstIndex < nums1.length && secondIndex < nums2.length) {
+        if (nums1[firstIndex] === nums2[secondIndex]) return nums1[firstIndex];
+        nums1[firstIndex] < nums2[secondIndex] ? firstIndex++ : secondIndex++;
     }
     return -1;
 };
