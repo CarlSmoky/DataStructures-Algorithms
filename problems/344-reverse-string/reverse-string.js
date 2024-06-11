aa835462833544fb8e4bc758f68b192c18032f60
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-06-11 09:05:23 -0400

    Time: 86 ms (21.78%) | Memory: 56.8 MB (63.24%) - LeetSync

---
+++ problems/344-reverse-string/reverse-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+const reverseString = (s) => {
+    let l = 0;
+    let r = s.length - 1;
+    while (l < r) {
+        const tmp = s[l];
+        s[l] = s[r];
+        s[r] = tmp;
+        l++;
+        r--;
+    }
+    return s;
+};
