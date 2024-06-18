89a7dc19749e2a48ed32a58e9697bf746628114d
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-06-18 11:47:37 -0400

    Time: 52 ms (65.07%) | Memory: 48.6 MB (88.06%) - LeetSync

--- problems/75-sort-colors/sort-colors.js
+++ problems/75-sort-colors/sort-colors.js
@@ -3,16 +3,14 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 const sortColors = (nums) => {
-    const count = new Map();
-    for (let n of nums) {
-        count.has(n) ? count.set(n, count.get(n) + 1) : count.set(n, 1);
-    }
-
-    let j = 0;
-    for (let x = 0 ; x < 3 ; x++){
-        for (let i = 0 ; i < count.get(x); i++){
-            nums[j] = x;
-            j++;
+    for(let i = 0 ;i < nums.length - 1;i++){
+        for(let j = 0 ; j < nums.length - i - 1;j++){
+            if(nums[j] > nums[j+1]){
+                const temp = nums[j]
+                 nums[j] = nums[j+1]
+                 nums[j+1] = temp
+            }
         }
     }
+    return nums
 };
