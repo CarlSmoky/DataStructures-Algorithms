58d7da8d993bf870f13813897e95b4d407deaeb8
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-07-18 12:39:21 -0400

    Time: 54 ms (80.66%) | Memory: 56.1 MB (5.07%) - LeetSync

--- problems/268-missing-number/missing-number.js
+++ problems/268-missing-number/missing-number.js
@@ -3,7 +3,15 @@
  * @return {number}
  */
 const missingNumber = (nums) => {
+    const hashMap = new Map();
+    for (let n of nums) {
+        if (!hashMap.get(n)) {
+            hashMap.set(n, true);
+        }
+    }
+    
+
     for (let i = 0; i < nums.length + 1; i++) {
-        if (nums.indexOf(i) === -1) return i;
+        if (!hashMap.get(i)) return i;
     }
 };
