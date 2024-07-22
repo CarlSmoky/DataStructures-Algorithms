0fb30e5a33e8f4daf9cc643e9136ff9b6dcb7619
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-07-22 13:42:36 -0400

    Time: 56 ms (75.28%) | Memory: 51.3 MB (80.33%) - LeetSync

--- problems/1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.js
+++ problems/1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.js
@@ -3,30 +3,24 @@
  * @return {number[]}
  */
 const luckyNumbers  = (matrix) => {
-    let rows = []
+    let minMax = new Set()
     for (let i = 0; i < matrix.length; i++) {
         let rowMin = Infinity;
         for (let j = 0; j < matrix[0].length; j++) {
             rowMin = Math.min(rowMin, matrix[i][j])
         }
-        rows.push(rowMin)
+        minMax.add(rowMin)
     }
     
-    let cols = []
     for (let j = 0; j < matrix[0].length; j++) {
         let colMax = - Infinity;
         for (let i = 0; i < matrix.length; i++) {
             colMax = Math.max(colMax, matrix[i][j])
         }
-        cols.push(colMax)
+        if (minMax.has(colMax)) {
+            return [colMax]
+        } 
     }
 
-    for (let r of rows) {
-        for (let c of cols) {
-            if (r === c){
-                return [r];
-            }
-        }
-    }
     return []
 };
