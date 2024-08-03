bec1d378ff612f910de83f52dd8826e6626bc389
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-08-03 14:59:21 -0400

    Time: 56 ms (85.56%) | Memory: 51.8 MB (30.56%) - LeetSync

--- problems/1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.js
+++ problems/1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.js
@@ -4,23 +4,17 @@
  * @return {boolean}
  */
 const canBeEqual = (target, arr) => {
-    const targetCount = new Map();
-    const arrCount = new Map();
-    for (let i = 0; i < target.length; i++) {
-        if (targetCount.get(target[i])) {
-            targetCount.set(target[i], targetCount.get(target[i]) + 1)
-        } else {
-            targetCount.set(target[i], 1)
-        }
-        if (arrCount.get(arr[i])) {
-            arrCount.set(arr[i], arrCount.get(arr[i]) + 1)
-        } else {
-            arrCount.set(arr[i], 1)
-        }
-    }
+    const count = new Map();
     
-    for (let [key, value] of targetCount) {
-        if (value !== arrCount.get(key)) return false;
+    for (let i = 0; i < target.length; i++) {
+        count.set(target[i], (count.get(target[i]) || 0) + 1);
+        count.set(arr[i], (count.get(arr[i]) || 0) - 1);
+    }
+
+    for (let n of count.values()) {
+        if (n !== 0) {
+            return false;
+        }
     }
     return true;
 };
