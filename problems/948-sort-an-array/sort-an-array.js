1e5bcdc8dae4fedad5575deb7fb43d2f478a0c14
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-08-08 10:05:36 -0400

    Time: 546 ms (16.44%) | Memory: 69.9 MB (62.43%) - LeetSync

---
+++ problems/948-sort-an-array/sort-an-array.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const sortArray = (nums) => {
+	const len = nums.length;
+	if (len < 2) return nums;
+
+	quickSort(nums, 0, len - 1)
+	return nums;
+};
+
+const quickSort = (nums, start, end) => {
+	if (start >= end) return;
+	let left = start
+    let right = end;
+	let pivot = nums[Math.floor((start+end) / 2)];
+	while(left <= right) {
+		while(left <= right && nums[left] < pivot){
+			left++
+		}
+		while(left <= right && nums[right] > pivot){
+			right--
+		}
+		if(left <= right){
+			let temp = nums[left]
+			nums[left] = nums[right]
+			nums[right] = temp
+			left++
+			right--
+		}
+	}
+	quickSort(nums, start, right)
+	quickSort(nums, left, end)
+}
