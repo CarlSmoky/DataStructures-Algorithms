bf6098de646eff9c3ed2e55dbc800388343952a2
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-08-20 10:40:57 -0400

    Time: 63 ms (12.14%) | Memory: 49.3 MB (53.94%) - LeetSync

--- problems/392-is-subsequence/is-subsequence.js
+++ problems/392-is-subsequence/is-subsequence.js
@@ -4,9 +4,8 @@
  * @return {boolean}
  */
 const isSubsequence = (s, t) => {
-    let sIndex = 0
-    let tIndex = 0
-    if (s.length === 0) return true;
+    let sIndex = 0;
+    let tIndex = 0;
     while (sIndex < s.length && tIndex < t.length) {
         if (s[sIndex] === t[tIndex]) {
             sIndex ++;
