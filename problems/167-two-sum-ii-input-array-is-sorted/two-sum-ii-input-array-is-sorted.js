819031803ee88f1599ffc4e577fba07c0842afc8
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-08-23 10:50:27 -0400

    Time: 53 ms (73.88%) | Memory: 50 MB (60.00%) - LeetSync

---
+++ problems/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (numbers, target) => {
+    let l = 0;
+    let r = numbers.length - 1;
+    while (l < r) {
+        const sum = numbers[l] + numbers[r]; 
+        if (sum === target) return [l + 1, r + 1];
+        sum > target ? r -- : l++
+    }
+};
