078a1d605045ab9edc163f7bd13da7c6ebffd2f9
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-08-24 22:30:27 -0400

    Time: 81 ms (35.29%) | Memory: 52.3 MB (99.15%) - LeetSync

--- problems/283-move-zeroes/move-zeroes.js
+++ problems/283-move-zeroes/move-zeroes.js
@@ -2,16 +2,15 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-const moveZeroes = (nums) => {
-    let left = 0;
-    let right = 0;
-    while (right < nums.length) {
-        if (nums[right] !== 0) {
-            const tmp = nums[right];
-            nums[right] = nums[left];
-            nums[left] = tmp;
-            left ++;
+const moveZeroes = nums => {
+    let l = 0;
+    
+    for (let r = 0; r < nums.length; r++) {
+        if (nums[r] !== 0) {
+            const tmp = nums[r];
+            nums[r] = nums[l];
+            nums[l] = tmp;
+            l ++;
         }
-        right ++;
     }
 };
