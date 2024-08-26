f93e215bb8ec67ba3cb3b5501d02eeaa9ae8c923
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-08-26 10:56:28 -0400

    Time: 90 ms (92.67%) | Memory: 61.9 MB (49.26%) - LeetSync

--- problems/1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.js
+++ problems/1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.js
@@ -4,18 +4,16 @@
  * @return {number}
  */
 const maxOperations = (nums, k) => {
-    let count = 0;
-    const map = new Map();
-
-    for (let num of nums) {
-        const complement = k - num;
-        if (map.get(complement) > 0) {
-            count++;
-            map.set(complement, map.get(complement) - 1);
+    let result = 0;
+    const hashMap = new Map();
+    for (let n of nums) {
+        const diff = k - n;
+        if (hashMap.get(diff) > 0) {
+            hashMap.set(diff, hashMap.get(diff) - 1);
+            result++;
         } else {
-            map.set(num, (map.get(num) || 0) + 1);
+            hashMap.set(n, (hashMap.get(n) || 0) + 1)
         }
     }
-    return count;
+    return result;
 };
-
