f220b654e87434ded40d5e0b9d37ae646f5a1054
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-09-03 11:32:59 -0400

    Time: 117 ms (90.92%) | Memory: 18 MB (60.26%) - LeetSync

--- problems/283-move-zeroes/move-zeroes.py
+++ problems/283-move-zeroes/move-zeroes.py
@@ -4,9 +4,8 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         r, l = 0, 0
-        while r < len(nums):
+        while l < len(nums) and r <len(nums):
             if nums[r] != 0:
                 nums[l], nums[r] = nums[r], nums[l]
                 l += 1
             r += 1
-            
