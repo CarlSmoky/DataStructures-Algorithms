7ca489c91a9e2883aa6d161986a83be5e4c35fa3
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-10-28 11:59:57 -0400

    Time: 0 ms (100.00%) | Memory: 49.2 MB (36.56%) - LeetSync

--- problems/1293-three-consecutive-odds/three-consecutive-odds.js
+++ problems/1293-three-consecutive-odds/three-consecutive-odds.js
@@ -3,7 +3,6 @@
  * @return {boolean}
  */
 const threeConsecutiveOdds = arr => {
-    let count = 0;
     for (let i = 0; i < arr.length - 2; i++) {
         if (isOdd(arr[i]) && isOdd(arr[i + 1]) && isOdd(arr[i + 2])) return true;
     }
