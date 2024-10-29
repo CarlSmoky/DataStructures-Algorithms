c07e005be1b702aa960f8e4505d646b70b559b7d
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-10-29 10:02:06 -0400

    Time: 43 ms (35.06%) | Memory: 65.6 MB (59.31%) - LeetSync

--- problems/15-3sum/3sum.js
+++ problems/15-3sum/3sum.js
@@ -2,34 +2,35 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-const threeSum = (nums) => {
+const threeSum = function(nums) {
     let result = [];
     nums.sort((a, b) => a - b);
-
+    
     for (let i = 0; i < nums.length; i++) {
-        if (i > 0 && nums[i] === nums[i-1]) {
+        // we don't want repeats, so skip numbers we've already seen
+        if (i > 0 && nums[i] === nums[i - 1]) {
             continue;
         }
-        
+
         let j = i + 1;
         let k = nums.length - 1;
 
         while (j < k) {
-            let total = nums[i] + nums[j] + nums[k];
+            let sum = nums[i] + nums[j] + nums[k];
 
-            if (total > 0) {
-                k--;
-            } else if (total < 0) {
-                j++;
+            if (sum > 0) {
+                k --;
+            } else if (sum < 0) {
+                j ++;
             } else {
                 result.push([nums[i], nums[j], nums[k]]);
-                j++;
+                j ++;
 
-                while (nums[j] === nums[j-1] && j < k) {
-                    j++;
+                while (nums[j] === nums[j - 1] && j < k) {
+                    j ++;
                 }
             }
         }
     }
-    return result;    
+    return result;
 };
