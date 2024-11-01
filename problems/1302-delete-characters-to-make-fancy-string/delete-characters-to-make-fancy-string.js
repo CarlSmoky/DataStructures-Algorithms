3a60bb03c93b9374cd05169796b5ae2646571be5
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-11-01 10:18:48 -0400

    Time: 68 ms (45.45%) | Memory: 60.1 MB (72.45%) - LeetSync

--- problems/1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.js
+++ problems/1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.js
@@ -3,11 +3,14 @@
  * @return {string}
  */
 const makeFancyString = (s) => {
-    let res = [...s]
-    for (let i = 0; i < res.length; i++) {
-        if (res[i] === res[i + 1] && res[i + 1] === res[i + 2]) {
-            res[i] = "";
+    let res = s[0]
+    for(let i = 1; i < s.length; i++){
+        if(s[i] === s[i - 1] && s[i] === s[i + 1]){
+            continue;
+        } else {
+            res += s[i];
         }
     }
-    return res.join("");
+
+    return res;
 };
