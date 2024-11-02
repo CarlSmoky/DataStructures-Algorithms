bfb642274d87ad04706f19f6b0cbf298a413d00b
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-11-02 08:45:42 -0400

    Time: 3 ms (66.45%) | Memory: 54.1 MB (81.00%) - LeetSync

--- problems/1046-max-consecutive-ones-iii/max-consecutive-ones-iii.js
+++ problems/1046-max-consecutive-ones-iii/max-consecutive-ones-iii.js
@@ -4,16 +4,15 @@
  * @return {number}
  */
  const longestOnes = (nums, k) => {
-    let countZero = 0;
     let l = 0
     let maxLength = 0;
     for (let r = 0; r < nums.length; r++) {
         if(nums[r] === 0) {
-            countZero ++;
+            k --;
         }
-        while (countZero > k) {
+        while (0 > k) {
             if (nums[l] === 0) {
-                countZero --
+                k ++;
             }
             l ++
         }
