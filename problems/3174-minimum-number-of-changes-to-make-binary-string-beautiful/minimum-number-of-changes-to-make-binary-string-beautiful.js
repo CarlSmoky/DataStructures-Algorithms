7bc9ed235fee5ea0fafcf4278016418dad56e80f
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-11-06 11:07:25 -0500

    Time: 4 ms (50.00%) | Memory: 51.2 MB (75.00%) - LeetSync

---
+++ problems/3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const minChanges = s => {
+    let count = 0;
+    for (let i = 0; i < s.length; i = i + 2) {
+        if (s[i] !== s[i + 1]) count ++;
+    }
+    return count;
+};
