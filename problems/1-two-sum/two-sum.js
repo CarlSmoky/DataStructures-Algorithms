b726c905cfde3348c317720b20f04d2c21280370
Kaoru Tsumita <81199269+CarlSmoky@users.noreply.github.com>
2024-11-25 10:39:34 -0500

    Time: 0 ms (100.00%) | Memory: 51 MB (38.97%) - LeetSync

--- problems/1-two-sum/two-sum.js
+++ problems/1-two-sum/two-sum.js
@@ -4,10 +4,10 @@
  * @return {number[]}
  */
 const twoSum = (nums, target) => {
-    const position = new Map();
+    const numMap = new Map();
     for (let i = 0; i < nums.length; i++) {
         const diff = target - nums[i];
-        if (position.has(diff)) return [position.get(diff), i];
-        position.set(nums[i], i);
+        if (numMap.has(diff)) return [numMap.get(diff), i]; 
+        numMap.set(nums[i], i);
     }
 };
